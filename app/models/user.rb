13ca3d1bc5f74052df66968ecc949e9b8698d4fe
Matias Canepa <macanepa@miuandes.cl>
2019-04-03 10:33:16 -0300

    Commit de la Vida Eterna

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :orders
+  validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/, message: "is invalid"}
+  validates :phone_number, format: {with: /\A[+]?(\d){9,12}+\z/, message: "is invalid, must contains only numbers (between 9 and 12). Could it start with '+'"}
+  validates :password, format: {with: /[0-9a-zA-Z]/, message: "is invalid, must contains only alphanumerics characters"}, length: {in: 8..12, message: "must be in range between 8 and 12 characters"}
 end
